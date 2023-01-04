744a8d330e95e5973405ccfad37edb658f7432ee
TomerGat <107539126+TomerGat@users.noreply.github.com>
2023-01-04 07:25:49 +0200

    Add files via upload

---
+++ web_app/server_header.py
@@ -0,0 +1,48 @@
+# imports:
+import http.server
+from http.server import BaseHTTPRequestHandler
+import socket
+import cgi
+import datetime
+import random
+import numpy as np
+from forex_python.converter import CurrencyRates
+import smtplib
+import multiprocessing
+import time
+
+
+# import objects from main file
+from server_dev import *
+
+
+# returns for savings account
+returns_premium = 4
+returns_medium = 2.75
+returns_minimum = 2
+
+# tables
+name_table = HashTable()  # account name table - {account name: serial number}
+number_table = HashTable()  # account number table - {account number: serial number}
+pass_table = HashTable()  # password table - {hash value of account password: serial number}
+phone_name_table = HashTable()  # account recovery recovery table - {hash value of phone number: account name}
+loc_type_table = HashTable()  # account type table - {serial number: type of account (reg/bus/sav)}
+value_summary_table = HashTable()  # value recovery table - {value summary: serial number}
+
+# Accounts log
+Accounts = Log()  # Log containing all Accounts
+
+# class containing data that can be accessible from all locations in file
+data = Global()
+
+# set containing all existing account numbers
+existing_account_numbers = set()
+
+# Dictionary of Logs containing history of received packets
+history = {}
+
+# set containing addresses connected
+addresses = set()
+
+# session timeout limit
+session_limit = 1200
