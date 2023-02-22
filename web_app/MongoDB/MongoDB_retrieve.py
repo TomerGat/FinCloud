3acaa166e5cb2db22c6e334b6e83729c00ad4672
TomerGat <107539126+TomerGat@users.noreply.github.com>
2023-02-22 12:17:39 +0200

    Add files via upload

--- web_app/MongoDB/MongoDB_retrieve.py
+++ web_app/MongoDB/MongoDB_retrieve.py
@@ -4,14 +4,48 @@ from Fincloud_data_structs import *
 from Fincloud_general_systems import *
 
 
+def create_trade_ledger_item(log):
+    trade_ledger_item = {}
+    for entry_index in range(len(log)):
+        trade_entry = log[entry_index]
+        trade_ledger_item[entry_index] = {
+            'currency from': trade_entry.cur_from,
+            'currency to': trade_entry.cur_to,
+            'amount': str(trade_entry.amount),
+            'conversion rate': trade_entry.conversion_rate,
+            'date': date_to_dict(trade_entry.date)
+        }
+    return trade_ledger_item
+
+
 def get_accounts_data(db):
     # function to create ledger from ledger item
     def item_to_ledger(item):
-        return 1  # continue
+        new_ledger = Log()
+        for entry_index in item.keys():
+            entry_data = item[entry_index]
+            action = entry_data['action type']
+            amount = entry_data['amount']
+            date = dict_to_date(entry_data['date'])
+            target_num = entry_data['other num']
+            target_dep = entry_data['other dep name']
+            entry_id = entry_data['entry id']
+            new_entry = Entry(action, amount, date, target_num, target_dep, entry_id)
+            new_ledger.append(new_entry)
+
+        return new_ledger
 
     # function to create ledger from ledger item
     def item_to_trade_ledger(item):
-        return 1  # continue
+        new_trade_ledger = Log()
+        for entry_index in item.keys():
+            trade_entry_data = item[entry_index]
+            cur_from = trade_entry_data['currency from']
+            cur_to = trade_entry_data['currency to']
+            amount = trade_entry_data['amount']
+            conversion = trade_entry_data['conversion rate']
+            date = dict_to_date(trade_entry_data['date'])
+            new_t_entry = TradeEntry(cur_from, cur_to, amount, date, conversion)
 
     # function to create ledger from ledger item
     def item_to_inbox(item):
@@ -47,7 +81,7 @@ def get_accounts_data(db):
         else:
             return
 
-        # Accounts.log.append(ac)
+        # Accounts.append(ac)
 
 
 def retrieve_data():
